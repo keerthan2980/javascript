8729966ba547d97f1c46d2dfa0e3dbe18041ba42
Bondugula Keerthan <bondugulakeerthan0102@gmail.com>
2024-07-21 20:05:05 +0530

    this is creation of class in js

---
+++ class.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+}
+const Person1=new Person("keerthan",21);
+const Person2=new Person("raju",23);
+console.log(Person1.name)
